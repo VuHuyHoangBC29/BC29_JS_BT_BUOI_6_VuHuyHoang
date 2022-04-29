0cbac2db021881dac65bdcac778cd31d34bd751a
VuHuyHoangBC29 <demigodkiller912@gmail.com>
2022-04-29 16:06:47 +0700

    source upload

---
+++ main.js
@@ -0,0 +1,65 @@
+/**
+ * Bài 1:
+ */
+var sum = 0;
+for (var i = 0; sum < 10000; i++) {
+    sum += i;
+    document.getElementById("footerBai1").innerHTML = i;
+}
+
+/**
+ * Bài 2
+ */
+function getEle(id) {
+    return document.getElementById(id);
+}
+
+getEle("btnBai2").onclick = function () {
+    var soX = getEle("soX").value * 1;
+    var soN = getEle("soN").value * 1;
+    var tong = 0;
+    for (j = 1; j <= soN; j++) {
+        tong += soX ** j;
+        // tong += Math.pow(soX, j)
+        getEle("footerBai2").innerHTML = tong;
+    }
+}
+
+/**
+ * Bài 3
+ */
+getEle("btnBai3").onclick = function () {
+    var soN_bai3 = getEle("soN_bai3").value * 1;
+    var giaiThua = 1;
+    for (k=1; k <= soN_bai3; k++) {
+        giaiThua *= k;
+        getEle("footerBai3").innerHTML = "Giai thừa: " + giaiThua;
+    }
+}
+
+/**
+ * Bài 4
+ */
+getEle("taoDiv").style.border = "black 5px solid";
+getEle("btnBai4").onclick = function () {
+    var content = "";
+    var divNho = document.querySelector('.divNho');
+    for (l = 1; l <= 10; l++) {
+        // content += "<div class=divNho style = height:30px></div>";
+        if (l % 2 === 0) {
+            content += "<div class=divNho style = height:30px></div>";
+            divNho.style.backgroundColor = 'yellow';
+
+        } else if (l % 2 !== 0) {
+            content += "<div class=divNho style = height:30px></div>";
+            divNho.style.backgroundColor = 'red';
+        }
+    }
+    // if (l % 2 === 0) {
+    //     divNho.style.backgroundColor = 'yellow';
+
+    // } else if (l % 2 !== 0) {
+    //     divNho.style.backgroundColor = 'red';
+    // }
+    getEle("taoDiv").innerHTML = content;
+}
